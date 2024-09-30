4cc7dc90f53b016e8a14c51ed3357f6c96fa3cfd
Vivek Vaishnani <vivek.vaishnani@deltafixes.com>
2024-09-30 18:00:45 +0530

    code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import { useContext, useEffect } from 'react';
+import { lazy, Suspense, useContext, useEffect } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
+import LogoLoader from './components/loader/loader-logo';
 import Splash from "./components/splash/splash";
 import ThemeSettingButton from "./components/theme-setting-button/theme-setting-button";
 import TopButton from './components/top-button/top-button';
 import ThemeContext from "./context/ThemeContext";
-import Contact from "./pages/contact/contact";
-import Education from "./pages/education/education";
-import Error404 from "./pages/error404/error404";
-import Experience from "./pages/experience/experience";
-import Home from "./pages/home/home";
-import Projects from "./pages/projects/projects";
 import { settings } from "./utils/utils";
+
+// Lazy load pages to optimize performance
+const Contact = lazy(() => import('./pages/contact/contact'));
+const Education = lazy(() => import('./pages/education/education'));
+const Error404 = lazy(() => import('./pages/error404/error404'));
+const Experience = lazy(() => import('./pages/experience/experience'));
+const Home = lazy(() => import('./pages/home/home'));
+const Projects = lazy(() => import('./pages/projects/projects'));
 
 function App() {
   const { theme } = useContext(ThemeContext);
   const location = useLocation();
-
 
   useEffect(() => {
     // Dynamically apply theme using CSS variables
@@ -31,16 +33,18 @@ function App() {
   return (
     <>
       {!isSplashOrHome && <Header />}
-      <Routes>
-        <Route path="/" element={settings.isSplash ? <Splash /> : <Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/contact" element={<Contact />} />
-        {settings.isSplash && (<Route path="/splash" element={<Splash />} />)}
-        <Route path="/projects" element={<Projects />} />
-        <Route path="*" element={<Error404 />} />
-      </Routes>
+      <Suspense fallback={<LogoLoader />}>
+        <Routes>
+          <Route path="/" element={settings.isSplash ? <Splash /> : <Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/contact" element={<Contact />} />
+          {settings.isSplash && (<Route path="/splash" element={<Splash />} />)}
+          <Route path="/projects" element={<Projects />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </Suspense>
       {!isSplashOrHome && <Footer />}
       {!isSplashOrHome && <TopButton />}
       {!isSplashOrHome && <ThemeSettingButton />}
